e2808d3eb665935061bae64c2dff8686f8569b4b
Alex Huryn <algurin77@gmail.com>
2026-07-26 16:35:15 +0200

    completed ci build 2

--- service/routes.py
+++ service/routes.py
@@ -55,6 +55,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -138,12 +139,9 @@ def delete_accounts(account_id):
 delete_account = delete_accounts
 
 
-
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
